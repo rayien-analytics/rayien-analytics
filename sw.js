e03cc332d70d972f3e3a8fe43669b5c48c6e5ca0
rayien-analytics <onyangowycliffe81@gmail.com>
2026-05-14 23:34:10 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,17 +2,26 @@ const CACHE_NAME = 'rayien-v1';
 const urlsToCache = [
   '/rayien-analytics/',
   '/rayien-analytics/index.html',
-  '/rayien-analytics/manifest.json'
+  '/rayien-analytics/manifest.json',
+  '/rayien-analytics/icon-192.png',
+  '/rayien-analytics/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
